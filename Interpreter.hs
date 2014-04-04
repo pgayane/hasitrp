6ab45e95ff7288194cf4ab096efc1487a5ba5862
Gayane <pgayane@gmail.com>
2014-04-04 19:49:52 -0400

    tokenizer works for multidigit input

--- Interpreter.hs
+++ Interpreter.hs
@@ -1,7 +1,7 @@
 --module Interpreter (Expr(..), interp) where
 import Data.Char 
 
-data Token = TokOp Operator | TokNum Int
+data Token = TokOp Operator | TokNum Int | TokLB | TokRB
     deriving (Show, Eq)
 
 data Operator = Plus | Minus | Times | Div
@@ -13,7 +13,19 @@ tokenizer ('+':e) = ((TokOp Plus): tokenizer(e))
 tokenizer ('-':e) = ((TokOp Minus): tokenizer(e))
 tokenizer ('*':e) = ((TokOp Times): tokenizer(e))
 tokenizer ('/':e) = ((TokOp Div): tokenizer(e))
+tokenizer ('(':e) = (TokLB : tokenizer(e))
+tokenizer (')':e) = (TokRB : tokenizer(e))
 tokenizer (e0:e) | isSpace e0 = tokenizer e
-                | isDigit e0 = (TokNum (digitToInt e0) : tokenizer(e))
+                | isDigit e0 = tokenizeNum (e0:e)
 
-main = print $ tokenizer " 1 + 4 / 3 "
+
+getNum :: String -> (String, String)
+getNum [] = ([],[])
+getNum(e0:e) | isDigit e0 = let(num, rest) = getNum(e) in ((e0:num),rest)
+             | otherwise = ([],(e0:e))
+
+tokenizeNum :: String -> [Token]
+tokenizeNum (e0:e) = let(num, rest) = getNum(e) in TokNum(read (e0:num)) : tokenizer(rest)
+
+
+main = print $ tokenizer "(1-43) + 56 / 3 "
