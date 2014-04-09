53c0b234a26ff19cc3bbc7b98159c42f813d778f
PLR <plredmond@gmail.com>
2014-04-09 15:44:44 -0400

    mechanically simplified parser to use a single type with a few constructors

--- parse.hs
+++ parse.hs
@@ -1,13 +1,10 @@
 import Text.Printf (printf)
 import Debug.Trace (trace)
-import Control.Monad (forM_)
+import Control.Monad (forM)
 
 -- shared
-data Add = Plus | Minus
-         deriving (Show, Eq)
-
-data Mul = Times | Div
-         deriving (Show, Eq)
+data Add = Plus  | Minus deriving (Show, Eq)
+data Mul = Times | Div   deriving (Show, Eq)
 
 -- tokens
 data Token = TokAdd Add
@@ -18,19 +15,11 @@ data Token = TokAdd Add
            deriving (Show, Eq)
 
 -- ast
-data Expr  = Ee Expr Add Term
-           | Et Term
-           | Eempty Int
-           deriving(Show, Eq)
-
-data Term  = Tt Term Mul Factor
-           | Tf Factor
-           | Tempty Int
-           deriving(Show, Eq)
-
-data Factor = Fe Expr
-            | Fn Double
-            deriving(Show, Eq)
+data AST = Bin2 Add AST AST
+         | Bin1 Mul AST AST
+         | Num Double
+         | Empty
+         deriving (Show, Eq)
 
 -- parser helper: take the left argument when it is success otherwise it take the right argument
 eor :: Either a b -> Either a b -> Either a b
@@ -38,54 +27,42 @@ eor x y = case x of Right _ -> x
                     Left  _ -> y
 
 -- top level parsing function
-parse :: [Token] -> Either String (Expr, [Token])
-parse toks = parseExpr (Eempty undefined, toks)
+parse :: [Token] -> Either String (AST, [Token])
+parse toks = parseExpr (Empty, toks)
 
-parseExpr :: (Expr, [Token]) -> Either String (Expr, [Token])
-parseExpr state@(_,       []) = return state
-parseExpr state@(Eempty _, _) = either Left                 parseExpr $ parseExprEt state
-parseExpr state               = either (\_ -> return state) parseExpr $ parseExprEe state
+parseExpr :: (AST, [Token]) -> Either String (AST, [Token])
+parseExpr state@(_,    []) = return state
+parseExpr state@(Empty, _) = either Left                 parseExpr $ parseTerm state
+parseExpr state            = either (\_ -> return state) parseExpr $ parseExprEe state
 
--- parseExpr helper to produce the Et constructor
-parseExprEt :: (Expr, [Token]) -> Either String (Expr, [Token])
-parseExprEt (Eempty _, toka) = do
-    (term, tokb) <- parseTerm (Tempty undefined, toka)
-    return (Et term, tokb)
--- parseExpr helper to produce the Ee constructor
-parseExprEe :: (Expr, [Token]) -> Either String (Expr, [Token])
+parseExprEe :: (AST, [Token]) -> Either String (AST, [Token])
 parseExprEe (left, toka) = do
     (op, tokb) <- parseAdd toka
-    (right, tokc) <- parseTerm (Tempty undefined, tokb)
-    return (Ee left op right, tokc)
+    (right, tokc) <- parseTerm (Empty, tokb)
+    return (Bin2 op left right, tokc)
 
-parseTerm :: (Term, [Token]) -> Either String (Term, [Token])
+parseTerm :: (AST, [Token]) -> Either String (AST, [Token])
 parseTerm state@(_,       []) = return state
-parseTerm state@(Tempty _, _) = either Left                 parseTerm $ parseTermTf state
+parseTerm       (Empty, toks) = either Left                 parseTerm $ parseFactor toks
 parseTerm state               = either (\_ -> return state) parseTerm $ parseTermTt state
                                                                  
--- parseTerm helper to produce the Tt constructor
-parseTermTt :: (Term, [Token]) -> Either String (Term, [Token])
+parseTermTt :: (AST, [Token]) -> Either String (AST, [Token])
 parseTermTt (left, toka) = do
     (op, tokb) <- parseMul toka
     (right, tokc) <- parseFactor tokb
-    return (Tt left op right, tokc)
--- parseTerm helper to produce the Tf constructor
-parseTermTf :: (Term, [Token]) -> Either String (Term, [Token])
-parseTermTf (Tempty _, toka) = do
-    (factor, tokb) <- parseFactor toka
-    return (Tf factor, tokb)
+    return (Bin1 op left right, tokc)
 
-parseFactor :: [Token] -> Either String (Factor, [Token])
-parseFactor (TokNum n:toka) = return (Fn n, toka)
+parseFactor :: [Token] -> Either String (AST, [Token])
+parseFactor (TokNum n:toka) = return (Num n, toka)
 parseFactor (TokLB   :toka) = parseFactorFe toka
 parseFactor               _ = Left "Expected a grammatical factor"
 
 -- parseFactor helper to produce the Fe constructor
-parseFactorFe :: [Token] -> Either String (Factor, [Token])
+parseFactorFe :: [Token] -> Either String (AST, [Token])
 parseFactorFe toka = do
-    (expr, tokb) <- parseExpr (Eempty undefined, toka)
+    (expr, tokb) <- parseExpr (Empty, toka)
     case tokb of
-        TokRB:tokc -> return (Fe expr, tokc)
+        TokRB:tokc -> return (expr, tokc)
         _          -> Left "Expected a right paren"
 
 -- consume an operator in {+,-} from state
@@ -98,41 +75,25 @@ parseMul :: [Token] -> Either String (Mul, [Token])
 parseMul (TokMul op:ta) = Right (op, ta)
 parseMul              _ = Left "Expected a multiplicative operator"
 
-examples :: [[Token]]
-examples = [ [ TokNum 20
-             , TokAdd Minus
-             , TokNum 6
-             , TokAdd Plus
-             , TokNum 4
-             ]
-           , [ TokNum 20
-             , TokAdd Minus
-             , TokLB
-             , TokNum 6, TokAdd Plus, TokNum 4
-             , TokRB
-             ]
-           , [ TokNum 20
-             , TokAdd Minus
-             , TokNum 2, TokMul Times, TokNum 3
-             , TokAdd Plus
-             , TokNum 44, TokMul Div, TokNum 11
-             ]
-           , [ TokLB
-             , TokLB
-             , TokNum 2.0, TokAdd Plus, TokNum 3.0
-             , TokRB
-             , TokMul Times, TokNum 4.0
-             , TokAdd Plus
-             , TokNum 6.0
-             , TokAdd Plus
-             , TokNum 2.0, TokMul Times, TokNum 5.0
-             , TokRB
-             , TokMul Div, TokNum 4.0
-             ]
-           ]
+lexed_ :: [[Token]]
+lexed_ = [ [TokNum 20.0,TokAdd Minus,TokNum 6.0,TokAdd Plus,TokNum 4.0]
+         , [TokNum 20.0,TokAdd Minus,TokLB,TokNum 6.0,TokAdd Plus,TokNum 4.0,TokRB]
+         , [TokNum 20.0,TokAdd Minus,TokNum 2.0,TokMul Times,TokNum 3.0,TokAdd Plus,TokNum 44.0,TokMul Div,TokNum 11.0]
+         , [TokLB,TokLB,TokNum 2.0,TokAdd Plus,TokNum 3.0,TokRB,TokMul Times,TokNum 4.0,TokAdd Plus,TokNum 6.0,TokAdd Plus,TokNum 2.0,TokMul Times,TokNum 5.0,TokRB,TokMul Div,TokNum 4.0]
+         ]
 
-main = forM_ examples $ \toks -> do
-    print toks
-    print $ parse toks
+parsed_ :: [Either String (AST, [Token])]
+parsed_ = [ Right (Bin2 Plus (Bin2 Minus (Num 20.0) (Num 6.0)) (Num 4.0),[])
+          , Right (Bin2 Minus (Num 20.0) (Bin2 Plus (Num 6.0) (Num 4.0)),[])
+          , Right (Bin2 Plus (Bin2 Minus (Num 20.0) (Bin1 Times (Num 2.0) (Num 3.0))) (Bin1 Div (Num 44.0) (Num 11.0)),[])
+          , Right (Bin1 Div (Bin2 Plus (Bin2 Plus (Bin1 Times (Bin2 Plus (Num 2.0) (Num 3.0)) (Num 4.0)) (Num 6.0)) (Bin1 Times (Num 2.0) (Num 5.0))) (Num 4.0),[])
+          ]
+
+main :: IO ()
+main = do
+    results <- forM (zip lexed_ parsed_) $ \(toks, expected) -> do
+        let ast = parse toks
+        print $ ast == expected
+    return ()
 
 -- eof
